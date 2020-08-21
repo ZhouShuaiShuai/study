b071ba9ab8ee9b7ed4685628d825043faccaed65
Administrator <lm99999993@163.com>
2020-08-21 08:44:35 +0800

    security权限校验

---
+++ spring-security/securing-web/src/main/java/com/zhouyf/learn/SecurityWebApplication.java
@@ -0,0 +1,18 @@
+package com.zhouyf.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class SecurityWebApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SecurityWebApplication.class,args);
+    }
+}
---
+++ spring-security/security-swagger/src/main/java/com/zhouyf/learn/SecuritySwaggerApplication.java
@@ -0,0 +1,17 @@
+package com.zhouyf.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SecuritySwaggerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SecuritySwaggerApplication.class,args);
+    }
+}
